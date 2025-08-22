684de0f0c1abe90c4f666d5dbbb8cb57c3941594
cague <cague@192.168.1.36>
2025-08-21 20:44:48 -0500

    carga inicial

---
+++ src/proyecto01_clase2/Proyecto01_clase2.java
@@ -0,0 +1,23 @@
+package proyecto01_clase2;
+
+import java.util.Scanner;
+
+public class Proyecto01_clase2 {
+    public static void main(String[] args) {
+        int n1, n2, suma;
+        Scanner lectura = new Scanner(System.in);
+
+        // Entrada
+        System.out.print("Ingresar número 1: ");
+        n1 = lectura.nextInt();
+
+        System.out.print("Ingresar número 2: ");
+        n2 = lectura.nextInt();
+
+        // Proceso
+        suma = n1 + n2;
+
+        // Salida
+        System.out.println("La suma es: " + suma);
+    }
+}
